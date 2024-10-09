64c5dbd197231bdae84b614d563a662e487ea3a8
nknish <noahknish@gmail.com>
2024-10-09 13:39:15 -0400

    simplify scoring

--- src/Success.java
+++ src/Success.java
@@ -1,31 +1,40 @@
-public class Success {
-    private static int climateScore = 0;
-    private static int consumerScore = 0;
-    private static int civicScore = 0;
-    private static int money;
+public final class Success {
+    private static int cli = 0; // climate score
+    private static int con = 0; // consumer score
+    private static int civ = 0; // civic score
+    private static int money = 1000000; // money in the bank!
+    private static int month = 0; // month (0 is intro, 13 is the end of the game)
 
-    public static int getClimateScore() {
-        return climateScore;
+    /*
+     * each month, the player gets up to 10 points per category
+     * which can total up to 120 at the end (perfect score)
+     */
+    public static void updateScores(int cli, int con, int civ) {
+        Success.cli += cli;
+        Success.con += con;
+        Success.civ += civ;
     }
 
-    public static void setClimateScore(int climateScore) {
-        Success.climateScore = boundAndRound(climateScore);
+    //
+    public static int getCli() {
+        return normalize12(cli);
     }
 
-    public static int getConsumerScore() {
-        return consumerScore;
+    public static int getCon() {
+        return normalize12(con);
     }
 
-    public static void setConsumerScore(int consumerScore) {
-        Success.consumerScore = boundAndRound(consumerScore);
+    public static int getCiv() {
+        return normalize12(civ);
     }
 
-    public static int getCivicScore() {
-        return civicScore;
-    }
-
-    public static void setCivicScore(int civicScore) {
-        Success.civicScore = boundAndRound(civicScore);
+    // return a normalized version of the score based on the month
+    public static int normalize12(int score) {
+        if (month == 13)
+            return score; // for 'month 13', aka end of game
+        if (month == 0)
+            return 0; // for 'month 0', aka before the start
+        return score * 12 / month; // for during the game, calculate predicted total score
     }
 
     public static int getMoney() {
@@ -36,20 +45,14 @@ public class Success {
         Success.money = money;
     }
 
-    // overload with default bounds: [0, 100]
-    private static int boundAndRound(double input) {
-        return boundAndRound(input, 0, 100);
+    public static int getMonth() {
+        return money;
     }
 
-    // customizable bounds
-    private static int boundAndRound(double input, int lower, int upper) {
-        if (input < lower) {
-            return 0;
+    public static void nextMonth() {
+        month++;
+        if (month == 13) {
+            // TODO: how do we wrap up?
         }
-        if (input > upper) {
-            return 100;
-        }
-        return (int) Math.round(input);
     }
-
 }
