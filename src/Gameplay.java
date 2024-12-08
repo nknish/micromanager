c86c9e9098be177b7ac10cbdf8291a38152ac594
nknish <noahknish@gmail.com>
2024-12-08 17:06:04 -0500

    pick a response and see what happens!

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
 import java.util.List;
-import java.util.ArrayList;
 
 public final class Gameplay {
+    private Scanner k = new Scanner(System.in);
+    private final String gn = Micromanager.guide;
+    private final String pn = Micromanager.playerName;
+
     protected Gameplay() {
         for (Scenario s : Micromanager.scenarios) {
+            Success.nextMonth();
             runScenario(s);
         }
     }
 
     private void runScenario(Scenario s) {
         String[] responsesWithIndices = printScenarioAndReturnPossibleResponses(s);
-        List<Scenario.Outcome> outcomes = getResponseAndItsPossibleOutcomes(responsesWithIndices);
-        String outcomeKey = rollDice(outcomes);
-        calculateOutcome(outcomeKey);
-        printOutcome(outcomeKey);
+        List<Scenario.Outcome> outcomes = getResponseAndItsPossibleOutcomes(s, responsesWithIndices);
+        Scenario.Outcome outcome = rollDice(outcomes);
+        handleOutcome(outcome);
     }
 
     private String[] printScenarioAndReturnPossibleResponses(Scenario s) {
         // prompt
-        P.println(s.prompt, Micromanager.guide);
+        P.println(s.prompt, gn);
         if (s.responses.size() == 2) {
-            P.println("you only have two choices:", Micromanager.guide);
+            P.println("you only have two choices:", gn);
         } else {
-            P.println("you've got a handful of options:", Micromanager.guide);
+            P.println("you've got a handful of options:", gn);
         }
 
         // get all responses in order
@@ -39,25 +42,83 @@ public final class Gameplay {
                 P.println("......" + responseString);
         }
 
-        P.println("so. what's your choice?", Micromanager.guide);
-        P.println();
+        P.println("so. what's your choice?", gn);
 
         return responsesWithIndices;
     }
 
-    private List<Scenario.Outcome> getResponseAndItsPossibleOutcomes(String[] responses) {
-        return null;
+    private List<Scenario.Outcome> getResponseAndItsPossibleOutcomes(Scenario s, String[] responses) {
+        int responseNumber;
+        while (true) {
+            P.print("", pn);
+            String userInput = k.nextLine().strip();
+            try {
+                responseNumber = Integer.parseInt(userInput);
+                if (responseNumber == 0)
+                    throw new IndexOutOfBoundsException();
+                return s.responses.get(responses[responseNumber]);
+            } catch (NumberFormatException e) { // response is not an integer
+            } catch (IndexOutOfBoundsException e) { // response is out of bounds
+            }
+            P.println("try again. enter a number from the options above!", gn);
+        }
     }
 
-    private String rollDice(List<Scenario.Outcome> os) {
-        return null;
+    private Scenario.Outcome rollDice(List<Scenario.Outcome> os) {
+        int bar = 90; // how high of a score do you need to get an odds boost?
+
+        // check for odds boosts and implement them
+        for (Scenario.Outcome o : os) {
+            if (!o.oddsBooster.equals("none")) {
+                if (o.oddsBooster.equals("cli")) {
+                    if (Success.getCli() >= bar) {
+                        if (Math.random() <= o.odds) {
+                            return o;
+                        }
+                    }
+                } else if (o.oddsBooster.equals("con")) {
+                    if (Success.getCon() >= bar) {
+                        if (Math.random() <= o.odds) {
+                            return o;
+                        }
+                    }
+                } else if (o.oddsBooster.equals("civ")) {
+                    if (Success.getCiv() >= bar) {
+                        if (Math.random() <= o.odds) {
+                            return o;
+                        }
+                    }
+                }
+            }
+        }
+
+        // if none of the boosts came through, calculate it like normal
+        double res = Math.random();
+        for (Scenario.Outcome o : os) {
+            res -= o.odds;
+            if (res <= 0)
+                return o;
+        }
+
+        // backup (should never occur)
+        P.print("something went wrong", "system");
+        return os.get(0);
     }
 
-    private void calculateOutcome(String outcomeKey) {
+    private void handleOutcome(Scenario.Outcome o) {
+        // calculate success metrics
+        Success.updateScores(o.dCli, o.dCon, o.dCiv);
+        Success.setMoney(Success.getMoney() + o.dMoney);
 
-    }
-
-    private void printOutcome(String outcomeKey) {
-
+        // tell the player what happened
+        P.println(o.result, gn);
+        P.println("here's how you did on all key metrics:", gn);
+        P.println("......climate-friendliness score: " + o.dCli + "/10");
+        P.println("......consumer happiness score: " + o.dCon + "/10");
+        P.println("......civic engagement score: " + o.dCiv + "/10");
+        P.println("and here are your total scores:", gn);
+        P.println("......climate-friendliness score: " + Success.getCli());
+        P.println("......consumer happiness score: " + Success.getCon());
+        P.println("......civic engagement score: " + Success.getCiv());
     }
 }
--- src/Setup.java
+++ src/Setup.java
@@ -5,7 +5,7 @@ public final class Setup {
     protected Setup() {
         getScenarios();
         orderAndSelectScenarios();
-        printScenarios();  // comment out later
+        // printScenarios();
     }
 
     private void getScenarios() {
@@ -13,29 +13,28 @@ public final class Setup {
     }
 
     private void orderAndSelectScenarios() {
-        
         // out of many possible, pick 12
         // random order, except first and last
     }
 
-    private void printScenarios() {
-        for (Scenario s : Micromanager.scenarios) {
-            P.println("scenario prompt: " + s.prompt);
-            for (String r : s.responses.keySet()) {
-                P.println("response: " + r);
-                List<Scenario.Outcome> os = s.responses.get(r);
-                P.println("possible outcomes:");
-                for (Scenario.Outcome o : os) {
-                    P.println("\t** " + o.result + " **");
-                    P.println("\todds: " + o.odds + ", plus a bonus for good " + o.oddsBooster);
-                    P.println("\tdCiv: " + o.dCiv);
-                    P.println("\tdCli: " + o.dCli);
-                    P.println("\tdCon: " + o.dCon);
-                    P.println("\tdMoney: " + o.dMoney);
-                    P.println();
-                }
-            }
-            P.println();
-        }
-    }
+    // private void printScenarios() {
+    // for (Scenario s : Micromanager.scenarios) {
+    // P.println("scenario prompt: " + s.prompt);
+    // for (String r : s.responses.keySet()) {
+    // P.println("response: " + r);
+    // List<Scenario.Outcome> os = s.responses.get(r);
+    // P.println("possible outcomes:");
+    // for (Scenario.Outcome o : os) {
+    // P.println("\t** " + o.result + " **");
+    // P.println("\todds: " + o.odds + ", plus a bonus for good " + o.oddsBooster);
+    // P.println("\tdCiv: " + o.dCiv);
+    // P.println("\tdCli: " + o.dCli);
+    // P.println("\tdCon: " + o.dCon);
+    // P.println("\tdMoney: " + o.dMoney);
+    // P.println();
+    // }
+    // }
+    // P.println();
+    // }
+    // }
 }
