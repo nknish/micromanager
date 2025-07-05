7dc990b16890a4de23799eb2a01affa4aa62c3b0
nknish <noahknish@gmail.com>
2025-07-05 00:46:34 -0400

    give it a real outro

--- src/Outro.java
+++ src/Outro.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public final class Outro {
+    private Scanner k = new Scanner(System.in);
+
     protected Outro() {
         P.clear();
-        P.print("outro placeholder", "system");
+
+        P.println("thanks for playing micromanager!!! hopefully you enjoyed it and learned a lot.", "system");
+        P.print("[press enter to say 'i had a great time!'] ");
+        k.nextLine();
+
+        P.println();
+        P.println("the game is over, but read on to take a deeper dive into microgrids...", "system");
+        P.print("[press enter to continue] ");
+        k.nextLine();
+        P.clear();
+
+        P.println(R.getStatement());
+
     }
 }
